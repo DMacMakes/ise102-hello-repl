a49f701a91f5dbced07764a3f6a343779d7326ad
dmacmakes <dmacmakes@gmail.com>
2021-09-19 03:43:24 +0000

    ignored gitignore

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/*******************************************************
+* ISE102 C++ Template 
+********************************************************
+* A C++ template project for ise102 students. For 
+* details and usage see end of file
+* ------------------------------------------------------
+* Authors:  - Daniel McGillick at torrens 2021
+*******************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+ // When sending a string to cout, adding a \n to it
+ // will tell the console to move down one line.
+ cout << "Hello ise102 \n";
+}
+
+/******************************************************
+* Usage
+*******************************************************
+* This project provides access to several libraries and 
+* utilities including fmt, scanlib, a `delay()` function
+* and more.
+*------------------------------------------------------
+* Start a New Project: 
+* Fork a copy of this repl.
+*------------------------------------------------------
+* Compile and Run:  
+* Hit the run button.
+*------------------------------------------------------
+* Rename the Output File: 
+* Edit the ".replit" file and change "my-project" to 
+* your desired program name. Note you must change it 
+* in 2 places within the file.
+******************************************************/
+
